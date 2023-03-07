d0fc3f2f831d55a78bffd41f219cbaca19b2d011
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-07 19:22:11 +0200

    added guess distribution chart

--- src/components/StatsContainer.tsx
+++ src/components/StatsContainer.tsx
@@ -1,11 +1,13 @@
+import translations from "@/lib/translations";
 import { PlayerData } from "@/pages";
 
 interface Props {
   playerData: PlayerData | null;
   className?: string;
+  locale: "fi" | "en";
 }
 
-const StatsContainer = ({ playerData, className }: Props) => {
+const StatsContainer = ({ playerData, className, locale }: Props) => {
   const winPercentage = () => {
     if (playerData?.games) {
       const games = Object.values(playerData.games);
@@ -16,7 +18,6 @@ const StatsContainer = ({ playerData, className }: Props) => {
     }
     return 0;
   };
-
 
   const currentStreak = () => {
     if (playerData?.games) {
@@ -33,7 +34,7 @@ const StatsContainer = ({ playerData, className }: Props) => {
       return streak;
     }
     return 0;
-  }
+  };
 
   const longestStreak = () => {
     if (playerData?.games) {
@@ -54,30 +55,75 @@ const StatsContainer = ({ playerData, className }: Props) => {
       return longestStreak;
     }
     return 0;
-  }
+  };
 
-  const totalGames = playerData?.games ? Object.keys(playerData.games).length : 0;
+  const totalGames = playerData?.games
+    ? Object.keys(playerData.games).length
+    : 0;
+
+  const calculateGuessDistributions = () => {
+    const obj: { [key: string]: number } = {
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0,
+      "5": 0,
+      "6": 0,
+    };
+    if (playerData?.games) {
+      const games = Object.values(playerData.games);
+      games.forEach((game) => {
+        if (game.word === game.guesses[game.guesses.length - 1]) {
+          obj[game.guesses.length]++;
+        }
+      });
+    }
+    return obj;
+  };
+
+  const guessDistributions = calculateGuessDistributions();
+  const maxValue = Math.max(...Object.values(guessDistributions));
 
   return (
     <div className="w-full">
-      <h1 className="font-semibold">TILASTOJA</h1>
-      <div className="flex flex-wrap justify-around md:justify-start md:flex-nowrap gap-10 whitespace-nowrap">
+      <hr className="my-5" />
+      <h1 className="font-semibold">{translations.statistics[locale]}</h1>
+      <div className="flex flex-wrap justify-around gap-10 whitespace-nowrap md:flex-nowrap md:justify-start">
         <div className="flex w-fit flex-col items-center">
           <p className="text-4xl font-bold">{totalGames}</p>
-          <p className="text-sm">Pelattu</p>
+          <p className="text-sm">{translations.gamesPlayed[locale]}</p>
         </div>
         <div className="flex w-fit flex-col items-center">
           <p className="text-4xl font-bold">{winPercentage()}</p>
-          <p className="text-sm">Voitto %</p>
+          <p className="text-sm">{translations.winPercentage[locale]}</p>
         </div>
         <div className="flex w-fit flex-col items-center">
           <p className="text-4xl font-bold">{currentStreak()}</p>
-          <p className="text-sm">Voittoputki</p>
+          <p className="text-sm">{translations.winStreak[locale]}</p>
         </div>
         <div className="flex w-fit flex-col items-center">
           <p className="text-4xl font-bold">{longestStreak()}</p>
-          <p className="text-sm">Pisin voittoputki</p>
+          <p className="text-sm">{translations.longestWinStreak[locale]}</p>
         </div>
+      </div>
+      <hr className="my-5" />
+      <h2 className="font-semibold">GUESS DISTRIBUTION</h2>
+      <div className="flex flex-col gap-2">
+        {Object.keys(guessDistributions).map((key, index) => {
+          return (
+            <div className="flex" key={index}>
+              <p className="h-6 w-6 text-center">{key}</p>
+              <div
+                style={{
+                  width: `${(guessDistributions[key] / maxValue) * 95}%`,
+                }}
+                className="flex h-6 min-w-fit flex-row-reverse content-end bg-gray-300 px-2 text-black dark:bg-neutral-900 dark:text-white"
+              >
+                <p>{guessDistributions[key]}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
