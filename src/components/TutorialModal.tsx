c574488930bafc0f2291ee1c96ca4d3e02f8eab7
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-07 19:22:33 +0200

    added translations

--- src/components/TutorialModal.tsx
+++ src/components/TutorialModal.tsx
@@ -1,35 +1,51 @@
+import translations from "@/lib/translations";
 import CloseButton from "./CloseButton";
 import GuessContainer from "./GuessContainer";
-
 
 interface Props {
   toggleTutorial: () => void;
   className?: string;
+  locale: "fi" | "en";
 }
 
-const TutorialModal = ({ toggleTutorial, className }: Props) => {
+const TutorialModal = ({ toggleTutorial, locale, className }: Props) => {
   return (
-    <div className={`
-      container animate-rise relative
-      place-self-center bg-white dark:bg-neutral-800 dark:text-white
-      p-10 rounded-xl h-[80vh] text-black
-      overflow-y-auto`}>
-      <h1 className="text-2xl md:text-4xl font-bold">Kuinka peli toimii?</h1>
-      <CloseButton className="absolute right-0 top-0 mt-2 mr-2" onClick={toggleTutorial} />
-      <ul className="list-disc m-4 text-sm md:text-lg">
-        <li>Jokainen sana täytyy olla oikea 5-kirjainta pitkä Suomalainen sana</li>
-        <li>Arvauksen jälkeen, peli näyttää arvauksen, ja näyttää väreillä miten oikeassa arvauksesi oli.</li>
+    <div
+      className={`
+      container relative h-[80vh]
+      animate-rise place-self-center overflow-y-auto rounded-xl
+      bg-white p-10 text-black dark:bg-neutral-800
+      dark:text-white`}
+    >
+      <h1 className="text-2xl font-bold md:text-4xl">{translations.tutorialHeader[locale]}</h1>
+      <CloseButton
+        className="absolute right-0 top-0 mt-2 mr-2"
+        onClick={toggleTutorial}
+      />
+      <ul className="m-4 list-disc text-sm md:text-lg">
+        <li>
+          {translations.tutorialFirstParagraph[locale]}
+        </li>
+        <li>
+          {translations.tutorialSecondParagraph[locale]}
+        </li>
       </ul>
-      <h2 className="text-2xl md:text-4xl font-bold">Esimerkkejä</h2>
-      <div className="text-sm md:text-lg m-4">
+      <h2 className="text-2xl font-bold md:text-4xl">{translations.tutorialExamples[locale]}</h2>
+      <div className="m-4 text-sm md:text-lg">
         <GuessContainer chars="KISSA" correctWord="CBCDE" animate={false} />
-        <p>Tässä arvauksessa ei ollut yhtäkään oikeinta kirjainta. Peli kertoo jos kirjainta ei ole sanassa, jos sana jää harmaaksi arvauksen jälkeen.</p>
+        <p>
+          {translations.tutorialExamplesWrong[locale]}
+        </p>
         <GuessContainer chars="LAUTA" correctWord="PLOLL" animate={false} />
-        <p>Tässä arvauksessa on yksi oikea kirjain, mutta se ei ole oikeassa paikassa. Peli kertoo sen oranssilla värillä.</p>
+        <p>
+          {translations.tutorialExamplesAlmost[locale]}
+        </p>
         <GuessContainer chars="TERÄS" correctWord="TPOLD" animate={false} />
-        <p>Tässä arvauksessa T on sanassa ja oikeassa paikassa. Peli kertoo sen vihreällä värillä.</p>
+        <p>
+          {translations.tutorialExamplesCorrect[locale]}
+        </p>
         <GuessContainer chars="AAMUN" correctWord="AAMUN" animate={false} />
-        <p>Tämä arvaus oli täysin oikein</p>
+        <p>{translations.tutorialExamplesCorrectWord[locale]}</p>
       </div>
     </div>
   );
