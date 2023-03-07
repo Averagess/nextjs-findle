fa1b65bde003c16faa54720f90d292e16345c9b5
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-07 19:21:53 +0200

    added locale support

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import LocaleSwitch from "./LocaleSwitch";
 import ThemeSwitch from "./ThemeSwitch";
 import TutorialButton from "./TutorialButton";
 
 interface Props {
   className?: string;
   toggleTutorial: () => void;
+  locale: "fi" | "en";
 }
 
-const Navbar = ({ className, toggleTutorial }: Props) => {
+const Navbar = ({ className, toggleTutorial, locale }: Props) => {
   return (
     <nav
-      className={`grid w-screen border-b-2 border-black dark:border-gray-500 items-center justify-items-center bg-gray-300 p-4 text-black dark:bg-neutral-800 dark:text-blue-700 ${className}`}
-      >
-      <h1 className="select-none text-3xl font-semibold">Findle</h1>
-      <div className="absolute right-4 flex gap-2 items-center">
-        <ThemeSwitch />
-        <TutorialButton onClick={toggleTutorial} />
+      className={`grid w-screen items-center border-b-2 border-black bg-gray-300 p-4 text-black dark:border-gray-500 dark:bg-neutral-800 dark:text-blue-500 ${className}`}
+    >
+      <h1 className="select-none sm:justify-self-center text-3xl font-semibold">Findle</h1>
+      <div className="absolute right-4 flex items-center gap-2">
+        <ThemeSwitch locale={locale} />
+        <TutorialButton locale={locale} onClick={toggleTutorial} />
+        <LocaleSwitch locale={locale} />
       </div>
     </nav>
   );
