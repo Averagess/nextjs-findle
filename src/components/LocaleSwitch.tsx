27bfe12e633a1d75049837ff90876bda9defd78b
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-07 19:21:40 +0200

    created localeswitch

---
+++ src/components/LocaleSwitch.tsx
@@ -0,0 +1,37 @@
+import translations from "@/lib/translations";
+import { useRouter } from "next/router";
+
+const LocaleSwitch = ({ locale }: { locale: "fi" | "en" }) => {
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    router.push(router.pathname, router.pathname, { locale: e.target.value });
+  };
+  return (
+    <select
+      className="
+          h-10 border-2 border-white bg-gray-800
+          bg-opacity-25 text-black dark:border-black
+          dark:bg-gray-500 dark:bg-opacity-25 dark:text-white
+          rounded-lg"
+      title={translations.localeSwitchTitle.en}
+      onChange={handleChange}
+      value={locale}
+    >
+      <option
+        className="bg-gray-600 bg-opacity-25 text-black dark:bg-gray-500 dark:text-white"
+        value="fi"
+      >
+        Finnish
+      </option>
+      <option
+        className="bg-gray-600 bg-opacity-25 text-black dark:bg-gray-500 dark:text-white"
+        value="en"
+      >
+        English
+      </option>
+    </select>
+  );
+};
+
+export default LocaleSwitch;
