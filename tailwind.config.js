f9b0ee52ac4ec0a79c3e6c414f9f0bbcccdfe516
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-07 19:23:25 +0200

    updated deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         "flip": "flip 1s ease-in-out both",
         "rise": "rise 1s ease-in-out both",
         "bg-blur": "bg-blur 1s ease-in-out both",
-        // "to-transparent": "0.5s to-transparent 5s ease-in-out both",
+        "shake": "shake 1s ease-in-out both",
       },
       boxShadow: {
         "neumorphism": "18px 18px 38px #b6b6b6, -18px -18px 38px #ffffff",
@@ -33,10 +33,11 @@ module.exports = {
           "0%": { backdropFilter: "blur(0px)" },
           "100%": { backdropFilter: "blur(10px)" },
         },
-        // "to-transparent": {
-        //   "0%": { opacity : "1" },
-        //   "100%": { opacity : "0" },
-        // },
+        "shake": {
+          "0%, 100%": { transform: "translateX(0)" },
+          "10%, 30%, 50%, 70%, 90%": { transform: "translateX(-10px)" },
+          "20%, 40%, 60%, 80%": { transform: "translateX(10px)" },
+        },
       },
     },
     plugins: [],
