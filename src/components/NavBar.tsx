c1a332c64efe32a59bab4e842eb683f53b80b632
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-09 17:02:07 +0200

    removed unneeded props, made it flex

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import LocaleSwitch from "./LocaleSwitch";
+import StatsButton from "./StatsButton";
 import ThemeSwitch from "./ThemeSwitch";
 import TutorialButton from "./TutorialButton";
 
 interface Props {
-  className?: string;
   toggleTutorial: () => void;
+  toggleStatistics: () => void;
   locale: "fi" | "en";
 }
 
-const Navbar = ({ className, toggleTutorial, locale }: Props) => {
+const Navbar = ({ toggleTutorial, toggleStatistics, locale }: Props) => {
   return (
     <nav
-      className={`grid w-screen items-center border-b-2 border-black bg-gray-300 p-4 text-black dark:border-gray-500 dark:bg-neutral-800 dark:text-blue-500 ${className}`}
+      className="
+      flex flex-col items-center border-b-2
+      border-black bg-gray-300 p-4
+      text-black
+      dark:border-gray-500 dark:bg-neutral-800 dark:text-blue-500"
     >
-      <h1 className="select-none sm:justify-self-center text-3xl font-semibold">Findle</h1>
+      <h1 className="select-none self-start sm:self-center text-3xl font-semibold">Findle</h1>
       <div className="absolute right-4 flex items-center gap-2">
+        <StatsButton locale={locale} onClick={toggleStatistics} />
         <ThemeSwitch locale={locale} />
         <TutorialButton locale={locale} onClick={toggleTutorial} />
         <LocaleSwitch locale={locale} />
