cf576be4e2d652b54d2eaa577a1d7cc8669e1a1b
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-09 17:01:36 +0200

    made layout flex, and h, w to screen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="layout">
+    <div className="flex flex-col h-screen w-screen">
       {children}
     </div>
   )
