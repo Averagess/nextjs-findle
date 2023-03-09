91a1065c1ad13b355e10abde787fc545c8ab2142
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-09 17:03:27 +0200

    style change

--- src/components/TutorialButton.tsx
+++ src/components/TutorialButton.tsx
@@ -15,8 +15,8 @@ const TutorialButton = ({ onClick, locale }: Props) => {
         place-items-center
         rounded-full border-2 border-white dark:border-black
       bg-gray-800 bg-opacity-25
-        text-3xl text-black hover:cursor-pointer
-      dark:bg-gray-500 dark:bg-opacity-25 dark:text-white `}
+        text-3xl text-white hover:cursor-pointer
+      dark:bg-gray-500 dark:bg-opacity-25 dark:text-black `}
       title={translations.tutorialTooltip[locale]}
     >
       <p>?</p>
