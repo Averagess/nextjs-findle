400549c8a0b408b9260bae4c5001db44be4cac21
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-13 17:07:51 +0200

    created index.ts for components

---
+++ src/components/index.ts
@@ -0,0 +1,36 @@
+import BackgroundBlur from "./BackgroundBlur";
+import CloseButton from "./CloseButton";
+import EndingModal from "./EndingModal";
+import GuessContainer from "./GuessContainer";
+import Keyboard from "./Keyboard";
+import KeyboardRow from "./KeyboardRow";
+import Layout from "./Layout";
+import LocaleSwitch from "./LocaleSwitch";
+import Navbar from "./NavBar";
+import ReleaseTimer from "./ReleaseTimer";
+import SettingsButton from "./SettingsButton";
+import StatsButton from "./StatsButton";
+import StatsContainer from "./StatsContainer";
+import ThemeSwitch from "./ThemeSwitch";
+import TutorialButton from "./TutorialButton";
+import TutorialModal from "./TutorialModal";
+
+
+export {
+  BackgroundBlur,
+  CloseButton,
+  EndingModal,
+  GuessContainer,
+  Keyboard,
+  KeyboardRow,
+  Layout,
+  LocaleSwitch,
+  Navbar,
+  ReleaseTimer,
+  SettingsButton,
+  StatsButton,
+  StatsContainer,
+  ThemeSwitch,
+  TutorialButton,
+  TutorialModal,
+};
